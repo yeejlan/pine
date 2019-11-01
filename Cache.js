6b73dc16273f95b858fe168178670ec09ec0e9b8
louis yu <yf_hello@163.com>
2019-11-01 10:38:50 +0800

    add cache

---
+++ Cache.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const log = require('pino')()
+const {promisify} = require('util');
+
+class Cache {
+	constructor(app) {
+		this._app = app;
+		this._redisProvider = app.getConfig()['cache.storage.provider'];
+		this._cacheEnable = app.getConfig()['cache.enable'] == "true";
+		let client = app.get(this._redisProvider);
+		this._redis = client;
+		if(!this._redis) {
+			let func = {func: 'pine.Cache.constructor'};
+			log.warn(func, "Can not found redis provider: %s, cache disabled.", this._redisProvider);
+			this._cacheEnable = false;
+		}
+		this._cachePrefix = app.getConfig()['cache.prefix'];
+		this._cacheExpireSeconds = parseInt(app.getConfig()['cache.expire.seconds']) || 3600;
+
+		this.getAsync = promisify(client.get).bind(client);
+		this.delAsync = promisify(client.del).bind(client);
+	}
+
+	client() {
+		return this._redis;
+	}
+
+	async set(key, value, expireSeconds) {
+		expireSeconds = expireSeconds || this._cacheExpireSeconds;
+		return new Promise((resolve, reject) => {
+			this._redis.set(this._cachePrefix + key, value, 'EX', expireSeconds, (err, replies) => {
+				resolve(replies);
+			});
+		});
+	}
+
+	async get(key) {
+		return await this.getAsync(this._cachePrefix + key);
+	}
+
+	async delete(key) {
+		return await this.delAsync(this._cachePrefix + key);
+	}
+}
+
+exports.Cache = Cache;
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 "use strict";
 const {App} = require("./App");
 const {Config} = require("./Config");
+const {Cache} = require("./Cache");
 const {Router} = require("./Router");
 const {ResourceLoader} = require("./ResourceLoader");
 const {Server} = require("./Server");
 
 exports.App = App;
 exports.Config = Config;
+exports.Cache = Cache;
 exports.Router = Router;
 exports.ResourceLoader = ResourceLoader;
 exports.Server = Server;
