302d212d5e064ababd2f7c5be231f6d87ee94b21
louis yu <yf_hello@163.com>
2019-11-04 17:23:29 +0800

    handle sigterm, sigint

--- Server.js
+++ Server.js
@@ -4,8 +4,9 @@ const http = require('http');
 const log = require('pino')()
 
 class Server {
-	constructor(router) {
-		this.router = router
+	constructor(app, router) {
+		this.router = router;
+		this.app = app;
 	}
 
 	serve(port) {
@@ -14,6 +15,29 @@ class Server {
 		let server = http.createServer(function(request, response) {
 			router.dispatch(request, response);
 		});
+
+		let app = this.app;
+		let shutdown = function() {
+			server.close(function onServerClosed (err){
+				if (err) {
+					let func = {func: "pine.Server.serve"};
+					log.error(func, "server.close error: %s", err);
+					process.exit(1);
+				}else {
+					app.shutdown();
+				}
+			});
+		}
+		process.on('SIGTERM', function onSigterm () {
+			log.info('Got SIGTERM, server shutting down');
+			shutdown();
+		});
+
+		process.on('SIGINT', function onSigterm () {
+			log.info('Got SIGINT, server shutting down');
+			shutdown();
+		});
+
 		server.listen(port);
 		if(server.listening){
 			log.info("Server listen on %s", port);
